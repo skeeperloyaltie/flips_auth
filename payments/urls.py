295335232b04f3648c979bc6b1df095fcbbe0927
Godfrey Gudah <gugod254@gmail.com>
2025-05-15 11:56:19 +0300

    update payments API

--- payments/urls.py
+++ payments/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import UserSubscriptionStatusView, PaymentMethodsView, PaymentPageView, VerificationPageView, \
-    CheckUserSubscriptionView, VerifyPaymentAPIView, UserPaymentHistoryView
+from .views import UserSubscriptionStatusView, PaymentPageView, VerificationPageView, \
+    CheckUserSubscriptionView, VerifyPaymentAPIView, UserPaymentHistoryView, InitiatePaymentAPIView
 
 urlpatterns = [
     path('verify-subscription/', UserSubscriptionStatusView.as_view(), name='verify-subscription'),
-    path('methods/', PaymentMethodsView.as_view(), name='payment-methods'),
+    path('methods/', InitiatePaymentAPIView.as_view(), name='payment-methods'),
     path('verification/', VerificationPageView.as_view(), name='verification-page'),
     path('check-subscription/', CheckUserSubscriptionView.as_view(), name='check-subscription'),
     path('verify-payment/', VerifyPaymentAPIView.as_view(), name='verify-payment'),
