f12a25f2dbd276a7634264fc4dcb2cfda9ae2101
Godfrey Gudah <gugod254@gmail.com>
2025-05-15 12:01:10 +0300

    probablity

--- payments/urls.py
+++ payments/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path('verification/', VerificationPageView.as_view(), name='verification-page'),
     path('check-subscription/', CheckUserSubscriptionView.as_view(), name='check-subscription'),
     path('verify-payment/', VerifyPaymentAPIView.as_view(), name='verify-payment'),
-    path('initiate/', PaymentMethodsView.as_view(), name='initiate-payment'),
-    path('verify-payment/', VerifyPaymentAPIView.as_view(), name='verify-payment'),
+    path('initiate/', InitiatePaymentAPIView.as_view(), name='initiate-payment'),
     path('history/', UserPaymentHistoryView.as_view(), name='payment-history'),
     path('payment-page/', PaymentPageView.as_view(), name='payment-page'),
 ]
