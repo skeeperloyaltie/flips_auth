cfcd0752166d9fea4c7416821d12606efe7ad541
Godfrey Gudah <gugod254@gmail.com>
2025-05-16 19:25:24 +0300

    Update urls.py

--- subscription/urls.py
+++ subscription/urls.py
@@ -5,6 +5,8 @@ from .views import SubscriptionPlanListCreateView, check_user_subscription, subs
 
 urlpatterns = [
     path('plans/', SubscriptionPlanListCreateView.as_view(), name='subscription-plans'),
+    path('plans/<int:id>/', SubscriptionPlanDetailView.as_view(), name='plan-detail'),
+
     path('status/', check_user_subscription, name='check-subscription-status'),
     path('subscribe/', subscribe, name='subscribe'),
     path('get-dashboard-url/', get_dashboard_url, name='get-dashboard-url'),
