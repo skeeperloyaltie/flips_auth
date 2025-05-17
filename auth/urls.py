9ffc6383b7dcb8dc44eba674a394eb9e953f400a
Godfrey Gudah <gugod254@gmail.com>
2025-05-17 17:19:54 +0300

    update

--- auth/urls.py
+++ auth/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 # from .views import ExampleView
 # Add this in your project's urls.py
@@ -54,3 +55,10 @@ urlpatterns = [
 
 ]
 # pprint(get_resolver().reverse_dict)
+
+from django.conf.urls.static import static
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -14,9 +14,3 @@ urlpatterns = [
 ]
 
 
-from django.conf.urls.static import static
-# Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    
